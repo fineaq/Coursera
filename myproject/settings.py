76780044577d148aa67d65f6e9f821e1a5cbf558
fineaq <fainaqlia@gmail.com>
2025-01-02 16:42:36 +0700

    V1.1-01.02.2025 Commit origin

--- myproject/settings.py
+++ myproject/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
